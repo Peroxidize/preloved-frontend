cf703c7b0bb6dd614515f566a8ad7ce8f7683da6
jasperket <jasperkeith1.vallecera@gmail.com>
2023-12-04 17:52:32 +0800

    Finished shop docs, waiting for API

---
+++ src/components/fragments/FormInputs/FileInput/Button.tsx
@@ -0,0 +1,16 @@
+import classes from "../../../../assets/componentCSS/formComponents/Button.module.css";
+
+interface ButtonProps {
+  text: string;
+  handleClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, handleClick }) => {
+  return (
+    <button className={classes.button} onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/fragments/FormInputs/FileInput/ImageInput.tsx
@@ -0,0 +1,51 @@
+import classes from "../../../../assets/componentCSS/formComponents/ImageInput.module.css";
+import imageIcon from "../../../../assets/icons/imageIcon.svg";
+
+interface ImageInputProps {
+  name: string;
+  onChange: (files: FileList) => void;
+  label?: string;
+  fileName?: string;
+  photo?: string | null;
+}
+
+const ImageInput: React.FC<ImageInputProps> = ({
+  name,
+  onChange,
+  label,
+  fileName,
+  photo,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(event.target.files!);
+    }
+  };
+
+  return (
+    <div className={classes.uploadContainer}>
+      <label htmlFor={name}>
+        <p className={classes.fileLabel}>{label}</p>
+        <div className={classes.uploadHere}>
+          <img
+            src={photo ? photo : imageIcon}
+            alt={photo ? "Photo uploaded" : "Upload here"}
+            className={classes.imageIcon}
+          />
+          <p>{fileName ? `Uploaded ${fileName}` : "Upload here"}</p>
+        </div>
+      </label>
+      <input
+        type="file"
+        name={name}
+        id={name}
+        className={classes.fileInput}
+        onChange={handleChange}
+        accept="image/*"
+        multiple={false}
+      />
+    </div>
+  );
+};
+
+export default ImageInput;
