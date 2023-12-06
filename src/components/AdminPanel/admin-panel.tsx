50696b8c6e9e85eca24507f6fb2bbbc197be0dfa
MarionEmmanuelBuhat <marionemmanuelbuhat@gmail.com>
2023-12-06 16:43:29 +0800

    vercel chuchu #2

--- src/components/AdminPanel/admin-panel.tsx
+++ src/components/AdminPanel/admin-panel.tsx
@@ -4,10 +4,10 @@ import axios from "axios";
 
 const domain = 'https://prelovedbackends.azurewebsites.net/';
 const pending = 'auth/verification/get_list_pending';
-const approvereject = 'auth/verification/approve_or_reject';
-const details = 'auth/verification/get_shop_owner_details';
-const status = 'auth/verification/document_status';
-const image = 'auth/verification/get_image';
+// const approvereject = 'auth/verification/approve_or_reject';
+// const details = 'auth/verification/get_shop_owner_details';
+// const status = 'auth/verification/document_status';
+// const image = 'auth/verification/get_image';
 
 export default function() {
 
